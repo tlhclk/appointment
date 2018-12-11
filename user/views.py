6e486936da3c0e32ef4f4f9a32208438bcd59478
tlhclk <tlhclk1312@gmail.com>
2018-12-11 20:58:27 +0300

    with base.html tlhclk

--- user/views.py
+++ user/views.py
@@ -9,7 +9,7 @@ def user_list(request,user_id):
             user=User.objects.all()
             return render(request,'list_any.html',{'title':'User List','list1':user})
     else:
-        return redirect('http://127.0.0.2:8000/')
+        return redirect('https://tlhclk.pythonanywhere.com/')
 
 
 def user_add(user_dict):
